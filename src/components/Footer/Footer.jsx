6a9d689a811e91bbc9f31ae8a2bf6efccb9e888a
Saikiranbollu14 <saikiranbollu14@gmail.com>
2024-09-18 10:53:04 +0530

    fotterChange

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -31,7 +31,7 @@ const Footer = () => {
         ))}
       </div>
       <p className="mt-8 text-center text-sm tracking-wide text-gray-400">
-        &copy;compileTab. All rights reserved.
+        &copy;Saikiran. All rights reserved.
       </p>
     </div>
   );
